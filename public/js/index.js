dea8ed07d709c105f4a516cd1992b6b31b6664bd
zhucy <1109927601@qq.com>
2018-05-13 12:17:14 +0800

    解决音频移动端不能播放问题

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
-$(function() {
+$(function () {
+    autoPlayMusic();
+    audioAutoPlay();
     //设置起始日期
     countTime('2014/01/31 17:45', 'day', 'hour', 'minute', 'second');
     var days = $('#day').text();
@@ -18,7 +20,7 @@ $(function() {
     );
 
 
-    var start = function() {
+    var start = function () {
         var index = 0;
         var rate = 6000;
         $('#say').text(says[(index++) % says.length]);
@@ -32,3 +34,43 @@ $(function() {
     }();
 
 });
+
+function audioAutoPlay() {
+    var audio = document.getElementById('bg-music');
+    audio.play();
+    document.addEventListener("WeixinJSBridgeReady", function () {
+        audio.play();
+    }, false);
+}
+
+// 音乐播放
+function autoPlayMusic() {
+    // 自动播放音乐效果，解决浏览器或者APP自动播放问题
+    function musicInBrowserHandler() {
+        musicPlay(true);
+        document.body.removeEventListener('touchstart', musicInBrowserHandler);
+    }
+
+    document.body.addEventListener('touchstart', musicInBrowserHandler);
+
+    // 自动播放音乐效果，解决微信自动播放问题
+    function musicInWeixinHandler() {
+        musicPlay(true);
+        document.addEventListener("WeixinJSBridgeReady", function () {
+            musicPlay(true);
+        }, false);
+        document.removeEventListener('DOMContentLoaded', musicInWeixinHandler);
+    }
+
+    document.addEventListener('DOMContentLoaded', musicInWeixinHandler);
+}
+
+function musicPlay(isPlay) {
+    var media = document.querySelector('#bg-music');
+    if (isPlay && media.paused) {
+        media.play();
+    }
+    if (!isPlay && !media.paused) {
+        media.pause();
+    }
+}
